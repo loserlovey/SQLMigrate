2284b4ad6b36337020e44abbe34b14bd68747731
chenshaoyi <chenshaoyi_2000@hotmail.com>
2018-05-15 13:23:05 +0800

    initial version

---
+++ SQLMigrate/mainForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace SQLMigrate
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.dbList = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(252, 42);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(205, 34);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // dbList
+            // 
+            this.dbList.FormattingEnabled = true;
+            this.dbList.Location = new System.Drawing.Point(23, 42);
+            this.dbList.Name = "dbList";
+            this.dbList.Size = new System.Drawing.Size(192, 21);
+            this.dbList.TabIndex = 1;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 426);
+            this.Controls.Add(this.dbList);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox dbList;
+    }
+}
